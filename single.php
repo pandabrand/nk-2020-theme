81d7956d45b72e1cafd483ebb031f39312386fdd
pandabrand <pandabrand@gmail.com>
2021-02-10 22:47:24 -0600

    scroll on small laptops

--- single.php
+++ single.php
@@ -12,6 +12,16 @@
 $context         = Timber::context();
 $timber_post     = Timber::get_post();
 $context['post'] = $timber_post;
+$sidebar_context = array();
+
+// if ( 'mec-events' === $timber_post->post_type ) {
+	// $page                              = get_page_by_path( 'calendar' );
+	// $sidebar_context['featured_image'] = new Timber\Image( get_post_thumbnail_id( $page->ID ) );
+	// $sidebar_context['title']          = $timber_post->post_title;
+	// $sidebar_context['type']           = $timber_post->post_type;
+// }
+
+// $context['sidebar'] = Timber::get_sidebar( 'sidebar.php', $sidebar_context );
 
 if ( post_password_required( $timber_post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
